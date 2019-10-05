967607d70f1c15b0aafe503967cf6bf5ff942039
codrelphi <chancerel.codjovi@gmail.com>
2019-10-05 18:05:08 +0100

    update the date field of my personal header

--- summer_2019_analysis.py
+++ summer_2019_analysis.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 #=================================================================================
 # author: Chancerel Codjovi (aka codrelphi)
-# date: 2019-09-20
+# date: 2019-10-05
 # description: basic analysis on my playlist of the summer 2019
 #              basic analysis done just for fun.
 #=================================================================================
