f2d221f73a55dcc93f024f2586f55430926e49fc
codrelphi <chancerel.codjovi@gmail.com>
2019-10-05 18:06:18 +0100

    add my personal header to the file

---
+++ data_playlist_summer_2019.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+#=================================================================================
+# author: Chancerel Codjovi (aka codrelphi)
+# date: 2019-10-05
+# description: contains data (songs) of my playlist of the summer 2019
+#=================================================================================
