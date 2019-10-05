9b8f85e8627a2a5fed1d0420d28e435a82cd3179
codrelphi <chancerel.codjovi@gmail.com>
2019-10-05 17:58:27 +0100

    add my personal header + description into the file created

---
+++ summer_2019_analysis.py
@@ -0,0 +1,8 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+#=================================================================================
+# author: Chancerel Codjovi (aka codrelphi)
+# date: 2019-09-20
+# description: basic analysis on my playlist of the summer 2019
+#              basic analysis done just for fun.
+#=================================================================================
