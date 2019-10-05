7c7e24aba315e7305dffd7cd3d679425b57bff6d
codrelphi <chancerel.codjovi@gmail.com>
2019-10-05 18:42:45 +0100

    add gospel songs into the file

--- data_playlist_summer_2019.py
+++ data_playlist_summer_2019.py
@@ -5,3 +5,127 @@
 # date: 2019-10-05
 # description: contains data (songs) of my playlist of the summer 2019
 #=================================================================================
+
+SONGS = [
+
+    {
+        'title': '10 000 reasons (Bless the Lord)',
+        'singer': ['Matt Redman'],
+        'style': None,
+        'year': None,
+        'Country': None
+    },
+    {
+        'title': 'Above all',
+        'singer': ['Michael W. Smith'],
+        'style': None,
+        'year': None,
+        'Country': None
+    },
+    {
+        'title': 'Awesome God',
+        'singer': ['Michael W. Smith'],
+        'style': None,
+        'year': None,
+        'Country': None
+    },
+    {
+        'title': 'Chevalier de Dieu',
+        'singer': ['Bebbi Philip'],
+        'style': None,
+        'year': None,
+        'Country': None
+    },
+    {
+        'title': 'Soon and very soon',
+        'singer': ['Brenda Fassie'],
+        'style': None,
+        'year': None,
+        'Country': None
+    },
+    {
+        'title': 'My trust is in you',
+        'singer': ['David G.'],
+        'style': None,
+        'year': None,
+        'Country': None
+    },
+    {
+        'title': 'God will make a way',
+        'singer': ['Don Moen'],
+        'style': None,
+        'year': None,
+        'Country': None
+    },
+    {
+        'title': 'Imela (Thank you)',
+        'singer': ['Nathaniel Bassey', 'Enitan Adaba'],
+        'style': None,
+        'year': None,
+        'Country': None
+    },
+    {
+        'title': 'Way maker',
+        'singer': ['Sinach'],
+        'style': None,
+        'year': None,
+        'Country': None
+    },
+    {
+        'title': 'You are great',
+        'singer': ['Steve Crown'],
+        'style': None,
+        'year': None,
+        'Country': None
+    },
+    {
+        'title': 'Nara ekele',
+        'singer': ['Travis Greene', 'Tim Godfrey'],
+        'style': None,
+        'year': None,
+        'Country': None
+    },
+    {
+        'title': 'What a beautiful name',
+        'singer': ['Hillsong Worship'],
+        'style': None,
+        'year': None,
+        'Country': None
+    },
+]
+
+
+"""
+
+Mix (Before 2019)
+•	Laisse mon gibier – Amaral
+•	That's the way It is - Céline Dion
+•	The power of love - Céline Dion
+•	Because You Loved Me - Céline Dion
+•	I'm alive - Céline Dion
+•	Earth Song - Michael Jackson
+•	Heal The World - Michael Jackson
+•	Will You Be There - Michael Jackson
+•	You Are Not Alone - Michael Jackson
+•	We are the World - USA for Africa
+•	Case – Teni
+Mix (Year 2019)
+•	Mon Coupé décalé – Bebi Philip
+•	Fumi – Crisba ft. Sirano
+•	Spécialité Ivoirienne - Debordo Leekunfa
+•	Moto Moto – Dj Arafat
+•	Victoire Remix - Kerozen ft. Bracket
+•	Ça A Cuit – Ko-C
+•	N’oublie jamais – Venom Cascadeur ft. Kerozen
+•	Jeune Fort - Nikanor
+•	Bamanou (Hommage à Tohon Stan) - Queen Fumi ft Ralami
+•	Sans bruit - Revolution
+•	Reason with me - Rudeboy
+•	C’est pas Les gbés gbés gbés – Shan’l
+•	La vie là-bas - Toofan ft. Louane
+•	Ou lé - Toofan ft. Jacob Desvarieux, Kassav
+•	Uyo Meyo - Teni
+•	Gbese - Qdot
+
+
+"""
