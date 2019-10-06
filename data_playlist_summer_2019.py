2d277a0e26e24a34a57afb5629b857fb76cf1b17
codrelphi <chancerel.codjovi@gmail.com>
2019-10-06 10:13:36 +0100

    add the contents of 'Country' field to all songs

--- data_playlist_summer_2019.py
+++ data_playlist_summer_2019.py
@@ -13,73 +13,73 @@ SONGS = [
         'singer': ['Matt Redman'],
         'style': None,
         'year': None,
-        'Country': None
+        'Country': 'Angleterre'
     },
     {
         'title': 'Above all',
         'singer': ['Michael W. Smith'],
         'style': None,
         'year': None,
-        'Country': None
+        'Country': 'USA'
     },
     {
         'title': 'Awesome God',
         'singer': ['Michael W. Smith'],
         'style': None,
         'year': None,
-        'Country': None
+        'Country': 'USA'
     },
     {
         'title': 'Chevalier de Dieu',
         'singer': ['Bebbi Philip'],
         'style': None,
         'year': None,
-        'Country': None
+        'Country': "Côte d'Ivoire"
     },
     {
         'title': 'Soon and very soon',
         'singer': ['Brenda Fassie'],
         'style': None,
         'year': None,
-        'Country': None
+        'Country': 'Afrique du Sud'
     },
     {
         'title': 'My trust is in you',
         'singer': ['David G.'],
         'style': None,
         'year': None,
-        'Country': None
+        'Country': 'Nigéria'
     },
     {
         'title': 'God will make a way',
         'singer': ['Don Moen'],
         'style': None,
         'year': None,
-        'Country': None
+        'Country': 'USA'
     },
     {
         'title': 'Imela (Thank you)',
         'singer': ['Nathaniel Bassey', 'Enitan Adaba'],
         'style': None,
         'year': None,
-        'Country': None
+        'Country': 'Nigéria'
     },
     {
         'title': 'Way maker',
         'singer': ['Sinach'],
         'style': None,
         'year': None,
-        'Country': None
+        'Country': 'Nigéria'
     },
     {
         'title': 'You are great',
         'singer': ['Steve Crown'],
         'style': None,
         'year': None,
-        'Country': None
+        'Country': 'Nigéria'
     },
     {
-        'title': 'Nara ekele',
+        'title': 'Nara ekele mo',
         'singer': ['Travis Greene', 'Tim Godfrey'],
         'style': None,
         'year': None,
@@ -90,189 +90,195 @@ SONGS = [
         'singer': ['Hillsong Worship'],
         'style': None,
         'year': None,
-        'Country': None
+        'Country': 'Australie'
     },
     {
         'title': 'Laisse mon gibier',
         'singer': ['Amaral'],
         'style': None,
         'year': None,
-        'Country': None
+        'Country': "Côte d'Ivoire"
     },
     {
         'title': "That's the way It is",
         'singer': ['Céline Dion'],
         'style': None,
         'year': None,
-        'Country': None
+        'Country': 'Canada'
     },
     {
         'title': 'The power of love',
         'singer': ['Céline Dion'],
         'style': None,
         'year': None,
-        'Country': None
+        'Country': 'Canada'
     },
     {
         'title': 'Because You Loved Me',
         'singer': ['Céline Dion'],
         'style': None,
         'year': None,
-        'Country': None
+        'Country': 'Canada'
     },
     {
         'title': "I'm alive",
         'singer': ['Céline Dion'],
         'style': None,
         'year': None,
-        'Country': None
+        'Country': 'Canada'
     },
     {
         'title': "Earth Song",
         'singer': ['Michael Jackson'],
         'style': None,
         'year': None,
-        'Country': None
+        'Country': 'USA'
     },
     {
         'title': "Heal The World",
         'singer': ['Michael Jackson'],
         'style': None,
         'year': None,
-        'Country': None
+        'Country': 'USA'
     },
     {
         'title': "Will You Be There",
         'singer': ['Michael Jackson'],
         'style': None,
         'year': None,
-        'Country': None
+        'Country': 'USA'
     },
     {
         'title': "You Are Not Alone",
         'singer': ['Michael Jackson'],
         'style': None,
         'year': None,
-        'Country': None
+        'Country': 'USA'
     },
     {
         'title': "We are the World",
         'singer': ['USA for Africa'],
         'style': None,
         'year': None,
-        'Country': None
+        'Country': 'USA'
     },
     {
         'title': "Case",
         'singer': ['Teni'],
         'style': None,
         'year': None,
-        'Country': None
+        'Country': 'Nigéria'
     },
-    ----------------------------------------------------------
     {
         'title': "Mon Coupé décalé",
         'singer': ['Bebi Philip'],
         'style': None,
-        'year': None,
-        'Country': None
+        'year': 2019,
+        'Country': "Côte d'Ivoire"
     },
     {
         'title': "Fumi",
         'singer': ['Crisba', 'Sirano'],
         'style': None,
-        'year': None,
-        'Country': None
+        'year': 2019,
+        'Country': 'Bénin'
     },
     {
         'title': "Spécialité Ivoirienne",
         'singer': ['Debordo Leekunfa'],
         'style': None,
-        'year': None,
-        'Country': None
+        'year': 2019,
+        'Country': "Côte d'Ivoire"
     },
     {
         'title': "Case",
         'singer': ['Teni'],
         'style': None,
-        'year': None,
-        'Country': None
+        'year': 2019,
+        'Country': 'Nigéria'
     },
     {
         'title': "Moto Moto",
         'singer': ['Dj Arafat'],
         'style': None,
-        'year': None,
-        'Country': None
+        'year': 2019,
+        'Country': "Côte d'Ivoire"
     },
     {
         'title': "Victoire Remix",
         'singer': ['Kerozen', 'Bracket'],
         'style': None,
-        'year': None,
-        'Country': None
+        'year': 2019,
+        'Country': "Côte d'Ivoire"
     },
     {
         'title': "Ça A Cuit",
         'singer': ['Ko-C'],
         'style': None,
-        'year': None,
-        'Country': None
+        'year': 2019,
+        'Country': 'Cameroun'
     },
     {
         'title': "N’oublie jamais",
         'singer': ['Venom Cascadeur', 'Kerozen'],
         'style': None,
-        'year': None,
-        'Country': None
+        'year': 2019,
+        'Country': "Côte d'Ivoire"
     },
     {
         'title': "Jeune Fort",
         'singer': ['Nikanor'],
         'style': None,
-        'year': None,
-        'Country': None
+        'year': 2019,
+        'Country': 'Bénin'
     },
     {
         'title': "Bamanou (Hommage à Tohon Stan)",
         'singer': ['Queen Fumi', 'Ralami'],
         'style': None,
-        'year': None,
-        'Country': None
+        'year': 2019,
+        'Country': 'Bénin'
     },
     {
         'title': "Sans bruit",
         'singer': ['Revolution'],
         'style': None,
-        'year': None,
-        'Country': None
+        'year': 2019,
+        'Country': "Côte d'Ivoire"
     },
     {
-        'title': "C’est pas Les gbés gbés gbés",
+        'title': "C’est pas les gbés gbés gbés",
         'singer': ["Shan'l"],
         'style': None,
-        'year': None,
-        'Country': None
+        'year': 2019,
+        'Country': 'Gabon'
     },
     {
         'title': "La vie là-bas",
         'singer': ['Toofan', 'Louane'],
         'style': None,
-        'year': None,
-        'Country': None
+        'year': 2019,
+        'Country': 'Togo'
+    },
+    {
+        'title': "Ou lé",
+        'singer': ['Toofan', 'Jacob Desvarieux'],
+        'style': None,
+        'year': 2019,
+        'Country': 'Togo'
     },
     {
         'title': "Uyo Meyo",
         'singer': ["Teni"],
         'style': None,
-        'year': None,
-        'Country': None
+        'year': 2019,
+        'Country': 'Nigéria'
     },
     {
         'title': "Gbese",
         'singer': ["Qdot"],
         'style': None,
-        'year': None,
-        'Country': None
-    },
+        'year': 2019,
+        'Country': 'Nigéria'
+    }
 ]
