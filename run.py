568f733cd70999f71c026efa6a98d4e9b0090a33
codrelphi <chancerel.codjovi@gmail.com>
2019-10-07 15:19:01 +0100

    add a basic running code: display all songs of the playlist

--- run.py
+++ run.py
@@ -5,3 +5,11 @@
 # date: 2019-10-07
 # description: file to run the program
 #=================================================================================
+import analysis.summer_2019
+from data.pl_summer_2019 import SONGS as sg
+
+
+for i in range(len(sg)):
+    for k, v in sg[i].items():
+        print('"{}" : {}'.format(k, v))
+    print('---------------------------------------------------------')
