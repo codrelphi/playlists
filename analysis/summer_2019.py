85830fdf652dabe3eecdee17b40dad516f6048e1
codrelphi <chancerel.codjovi@gmail.com>
2019-10-07 15:18:28 +0100

    remove the previous codes (contents)

--- analysis/summer_2019.py
+++ analysis/summer_2019.py
@@ -6,10 +6,3 @@
 # description: basic analysis on my playlist of the summer 2019
 #              basic analysis done just for fun.
 #=================================================================================
-from data_playlist_summer_2019 import SONGS
-
-
-for l in range(len(SONGS)):
-    for k, v in SONGS[l].items():
-        print('"{}" : {}'.format(k, v))
-    print('---------------------------------------------------------')
